092ffcdff2ef6dc9615ab582ec9e5d07f02d6844
Ati <190020009@iitdh.ac.in>
2021-07-18 17:44:28 +0530

    boiler plate

--- cpquickfind/src/Solution.java
+++ cpquickfind/src/Solution.java
@@ -73,55 +73,7 @@ public class Solution {
      while(i>0) res*=i--;
      return res;
   }
-  static double sum=0;
-  static void sol(double c,double m,double p,double v,double cum,double length) {
-	  if(c<0.000001)c=0;
-      if(m<0.000001)m=0;
-	  sum+= (p*cum*(length+1));
-	 if(c>=v) {
-		 if(m>0) {
-			 sol(c-v,m+(v/2),p+(v/2),v,cum*c,length+1);
-		 }
-		 else {
-			 sol(c-v,m,p+v,v,cum*c,length+1);
-		 }
-	 }
-	 else if(c>0 && c<=v) {
-		 if(m>0) {
-			 sol(0,m+c/2,p+c/2,v,cum*c,length+1);
-		 }
-		 else {
-			 sol(0,m,p+c,v,cum*c,length+1);
-		 }
-	 }
-	 if(m>=v) {
-		 if(c>0) {
-			 sol(c+v/2,m-v,p+v/2,v,cum*m,length+1);
-		 }
-		 else {
-			 sol(c,m-v,p+v,v,cum*m,length+1);
-		 }
-	 }
-	 else if(m>0 && m<=v) {
-		 if(c>0) {
-			 sol(c+m/2,0,p+m/2,v,cum*m,length+1);
-		 }
-		 else {
-			 sol(c,0,p+m,v,cum*m,length+1);
-		 }
-	 }
-  }
-
-  static int INF = (int)1e9+7;
-  
   public static void main(String[] args) {
    FastReader fs = new FastReader();
-   int tot = 0;
-   int n = fs.nextInt();
-   for (int i = 0; i < n; i++) {
-      tot+=fs.nextInt();
-   }
-   if(tot%n==0) System.out.println(n);
-   else System.out.println(n-1);
   }
 }
